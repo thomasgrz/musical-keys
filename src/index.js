33d928d77377e4697d64684ea82535f50865b9b3
thomas <thomasgrz@outlook.com>
2021-03-23 20:01:35 -0400

    test pre-commit hook

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-var anime = require('animejs').default;
+var anime = require("animejs").default;
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    var element = document.getElementById('section')
-    document.addEventListener("keydown", (e) => {
-        if(e.code === "KeyA"){
-            debugger
-            anime({
-                loop: 2,
-                        targets: [element], 
-                rotate: {
-                    value: 360,
-                          duration: 1800,
-                    easing: 'easeInOutSine'
-                },
-                scale: {
-                    value: 2,
-                    duration: 1600,
-                    delay: 800,
-                    easing: 'easeInOutQuart'
-                    },
-                delay: 250 // All properties except 'scale' inherit 250ms delay
-            })
-        }
-    })
-    
-    console.log("The DOM content has been loaded.")
-})
+document.addEventListener("DOMContentLoaded", () => {
+  var element = document.getElementById("section");
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "KeyA") {
+      debugger;
+      anime({
+        loop: 2,
+        targets: [element],
+        rotate: {
+          value: 360,
+          duration: 1800,
+          easing: "easeInOutSine",
+        },
+        scale: {
+          value: 2,
+          duration: 1600,
+          delay: 800,
+          easing: "easeInOutQuart",
+        },
+        delay: 250, // All properties except 'scale' inherit 250ms delay
+      });
+    }
+  });
+
+  console.log("The DOM content has been loaded.");
+});
